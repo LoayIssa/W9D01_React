ee6a956a29f0b7a444681671e497e93582a4a8b3
LoayIssa <issaloay680@gmail.com>
2021-05-23 20:57:01 +0300

    solved all pulse check

---
+++ src/components/ListItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const ListItem =({todo,id})=>{
+    const handleLi =()=>{
+        console.log(id)
+    }
+    return(<>
+    <li onClick = {handleLi}>{todo}</li>
+
+    </>)
+};
+export default ListItem
